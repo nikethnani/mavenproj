eadafddb83cc3c155f78198e66c8aaf566b42f67
nikethnani <44430154+nikethnani@users.noreply.github.com>
2019-04-29 17:42:35 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,6 +13,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
+        System.out.println("SSSSSSSSSSSS");
       System.out.println(new App().getMessage());
     }
 
