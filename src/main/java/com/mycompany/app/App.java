ff6483de47eb35f66165cdf8a1699fd37a6b0166
nikethnani <44430154+nikethnani@users.noreply.github.com>
2019-04-29 17:45:45 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,9 +6,9 @@ package com.mycompany.app;
 public class App
 {
     private final String message = "Hello World!";
-    String name = "AAAA"
-    String age = "33"
-     String address = "AAAAAAA"
+    String name = "AAAA";
+    String age = "33";
+     String address = "AAAAAAA";
 
     public App() {}
 
